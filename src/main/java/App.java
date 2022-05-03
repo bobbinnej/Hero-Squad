754fa9364cb7243c5194154b8b9fc901acd8b916
jackie <bobbinnej@gmail.com>
2022-05-03 15:51:29 +0300

    Create hero-details.hbs to cater ofr hero statistics

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,8 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
       staticFileLocation("/public");
+      Hero.setUpNewHero1();
+      Hero.setUpNewHero2();
       get("/", (request, response)->{
           Map<String, Object>model=new HashMap<String, Object>();
           return new ModelAndView(model, "index.hbs");
@@ -31,6 +33,20 @@ public class App {
           return new ModelAndView(model, "hero.hbs");
       },  new HandlebarsTemplateEngine());
 
+      post("/new/hero",(request,respond)->{
+          Map<String,Object>model=new HashMap<>();
+          Integer id=Integer.parseInt(request.queryParams("age"));
+          String name=request.queryParams("name");
+          Integer age=Integer.parseInt(request.queryParams("age"));
+          String power=request.queryParams("power");
+          String weakness=request.queryParams("weakness");
+
+          Hero newHero=new Hero(id,name,age,power,weakness);
+          request.session().attribute("myHeroes",name);
+          model.put("myHeroes",request.session().attribute("myHeroes"));
+          model.put("myHeroes",1);
+          return new ModelAndView(model,"success.hbs");
+      },  new HandlebarsTemplateEngine());
 
 
     }
